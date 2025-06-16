fef32442b04b818832d7239b67466972fa8971e7
yukti-says <150407207+Yukti2004@users.noreply.github.com>
2025-06-16 18:18:13 +0530

    added queue

---
+++ Queue/DoubleEndedQueqe.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+#define S 10;
+
+class Queue{
+    public:
+     int q[S];
+     int front , rear;
+     Queue(){
+        front = -1;
+        rear = -1;
+     }
+
+     bool isfull(){
+        
+     }
+
+     int
+};
+    
